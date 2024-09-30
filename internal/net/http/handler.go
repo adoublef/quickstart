0d54396633a1336e4f97674d6e62469fb776563e
Rahim <rahim.affulbrown@deltaflare.com>
2024-09-30 14:01:37 +0000

    refactor and tidy

--- internal/net/http/handler.go
+++ internal/net/http/handler.go
@@ -1,10 +1,14 @@
 package http
 
 import (
+	"crypto/tls"
 	"net/http"
+	"sync"
 	"time"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.tmp/quickstart/internal/runtime/debug"
+	// _ "golang.org/x/crypto/x509roots/fallback"
 )
 
 type Server = http.Server
@@ -23,14 +27,34 @@ func Handler(burst int, ttl time.Duration) http.Handler {
 
 	// todo: csrf
 	h := AcceptHandler(mux)
-	h = LimitHandler(mux, burst, ttl) // todo: throttle?
+	h = LimitHandler(h, burst, ttl) // todo: throttle?
 	h = otelhttp.NewHandler(h, "QuickStart")
 	return h
 }
 
 // handleReady serves as an endpoint to signal if the service alive.
 func handleReady() http.HandlerFunc {
+	var (
+		once sync.Once
+		err  error
+	)
 	return func(w http.ResponseWriter, r *http.Request) {
+		// make a https request
+		once.Do(func() {
+			var c = &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: false,
+					},
+				},
+			}
+			_, err = c.Get("https://google.com")
+			debug.Printf("handleReady: _, %v = http.Get(_)", err)
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		w.WriteHeader(200)
 	}
 }
